f270a2d8abeefa668f985ff10a4bd89f3cbe1e6c
Cierra Nease <45543866+cnease10@users.noreply.github.com>
2019-08-21 22:16:25 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+var cards = [{
+   rank: "queen",
+   suit: "hearts",
+   cardImage: "images/queen-of-hearts.png"
+}, {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+}, {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+}, {
+ rank: "king",
+ suit: "diamonds",
+ cardImage: "images/king-of-diamonds.png"
+} ];
+
+var cardsInPlay = [];
+
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else  {
+     console.log("Sorry, try again.");
+  };
+};
+
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
+
+  this.setAttribute("src",cards[cardId].cardImage);
+  console.log("User flipped" + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+
+  checkForMatch();
+};
+
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i );
+    cardElement.addEventListener("click", flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+createBoard();
